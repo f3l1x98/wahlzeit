9db92809af92bb89e1d5ef09d5ed3e29e3136b61
Felix Müller <felix.mueller0998@gmx.de>
2019-10-31 19:21:03 +0100

    added EmailAddressTest to EmailServiceTestSuite

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -6,14 +6,12 @@ import org.wahlzeit.handlers.*;
 import org.wahlzeit.model.*;
 import org.wahlzeit.model.persistence.*;
 import org.wahlzeit.services.*;
-import org.wahlzeit.services.mailing.*;
 import org.wahlzeit.utils.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         StringUtilTest.class,
         VersionTest.class,
-        EmailAddressTest.class,
         LogBuilderTest.class,
         EmailServiceTestSuite.class,
         AccessRightsTest.class,
---
+++ src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
@@ -0,0 +1,14 @@
+package org.wahlzeit.services;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.services.EmailAddressTest;
+import org.wahlzeit.services.mailing.EmailServiceTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        EmailServiceTest.class,
+        EmailAddressTest.class
+})
+public class EmailServiceTestSuite {
+}
