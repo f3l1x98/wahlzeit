97faa00e1e566511e76fb7a3a82e0b687e0e17bb
Felix Müller <felix.mueller0998@gmx.de>
2019-11-07 18:41:29 +0100

    Overriding some methods

--- src/main/java/org/wahlzeit/model/WaterfallPhotoFactory.java
+++ src/main/java/org/wahlzeit/model/WaterfallPhotoFactory.java
@@ -1,7 +1,54 @@
 package org.wahlzeit.model;
 
 
+import org.wahlzeit.services.LogBuilder;
+
+import java.util.logging.Logger;
+
 public class WaterfallPhotoFactory extends PhotoFactory {
+
+    private static final Logger log = Logger.getLogger(WaterfallPhotoFactory.class.getName());
+    /**
+     * Hidden singleton instance; needs to be initialized from the outside.
+     */
+    private static WaterfallPhotoFactory instance = null;
+
+    /**
+     *
+     */
+    protected WaterfallPhotoFactory() {
+        // do nothing
+    }
+
+    /**
+     * Hidden singleton instance; needs to be initialized from the outside.
+     */
+    public static void initialize() {
+        getInstance(); // drops result due to getInstance() side-effects
+    }
+
+    /**
+     * Public singleton access method.
+     */
+    public static synchronized WaterfallPhotoFactory getInstance() {
+        if (instance == null) {
+            log.config(LogBuilder.createSystemMessage().addAction("setting generic WaterfallPhotoFactory").toString());
+            setInstance(new PhotoFactory());
+        }
+
+        return instance;
+    }
+
+    /**
+     * Method to set the singleton instance of PhotoFactory.
+     */
+    protected static synchronized void setInstance(WaterfallPhotoFactory photoFactory) {
+        if (instance != null) {
+            throw new IllegalStateException("attempt to initalize WaterfallPhotoFactory twice");
+        }
+
+        instance = photoFactory;
+    }
 
     // TODO instance init -> no idea how exactly
 
@@ -18,5 +65,15 @@ public class WaterfallPhotoFactory extends PhotoFactory {
     @Override
     public WaterfallPhoto loadPhoto(PhotoId id) {
         return null;
+    }
+
+    @Override
+    public PhotoFilter createPhotoFilter() {
+        return super.createPhotoFilter();
+    }
+
+    @Override
+    public PhotoTagCollector createPhotoTagCollector() {
+        return super.createPhotoTagCollector();
     }
 }
--- src/main/java/org/wahlzeit/model/WaterfallPhotoManager.java
+++ src/main/java/org/wahlzeit/model/WaterfallPhotoManager.java
@@ -1,26 +1,58 @@
 package org.wahlzeit.model;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 public class WaterfallPhotoManager extends PhotoManager {
 
+    /**
+     *
+     */
+    protected static final WaterfallPhotoManager instance = new WaterfallPhotoManager();
 
-    /*@Override
+    private static final Logger log = Logger.getLogger(WaterfallPhotoManager.class.getName());
+
+    /**
+     * In-memory cache for photos
+     */
+    protected Map<PhotoId, WaterfallPhoto> photoCache = new HashMap<>();
+
+    /**
+     *
+     */
+    public WaterfallPhotoManager() {
+        photoTagCollector = WaterfallPhotoFactory.getInstance().createPhotoTagCollector();
+    }
+
+    @Override
     public WaterfallPhoto getPhotoFromId(PhotoId id) {
-        return super.getPhotoFromId(id);
+        //return super.getPhotoFromId(id); //TODO not sure if that can be used
+        if (id == null) {
+            return null;
+        }
+
+        WaterfallPhoto result = doGetPhotoFromId(id);
+
+        if (result == null) {
+            result = WaterfallPhotoFactory.getInstance().loadPhoto(id);
+            if (result != null) {
+                doAddPhoto(result);
+            }
+        }
+
+        return result;
     }
 
     @Override
     protected WaterfallPhoto doGetPhotoFromId(PhotoId id) {
-        return super.doGetPhotoFromId(id);
-    }
-
-    @Override
-    public Map<PhotoId, Photo> getPhotoCache() {
-        return super.getPhotoCache();
+        return photoCache.get(id);
     }
 
     @Override
     public WaterfallPhoto getVisiblePhoto(PhotoFilter filter) {
-        return super.getVisiblePhoto(filter);
-    }*/
+        filter.generateDisplayablePhotoIds();
+        return getPhotoFromId(filter.getRandomDisplayablePhotoId());
+    }
 }
